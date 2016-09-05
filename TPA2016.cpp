3bc46fb4379351eaa6d029f8b969fe8eecdfc63a
Matt Jenkins <matt@majenko.co.uk>
2016-09-05 20:45:05 +0100

    Initial import

---
+++ TPA2016.cpp
@@ -0,0 +1,58 @@
+#include <TPA2016.h>
+
+void TPA2016::begin() {
+    enableAmplifier();
+    dtwi->beginMaster();
+
+    setAmp(1, 0b11000000);
+    setAmp(2, 0x01);
+    setAmp(6, 0x80);
+    setAmp(7, 0xF0);
+    setGain(0);
+}
+
+void TPA2016::setAmp(uint8_t reg, uint8_t val) {
+    uint8_t state = 0;
+    while (1) {
+        switch (state) {
+            case 0: // begin write
+                if (dtwi->startMasterWrite(0x58)) {
+                    state = 1;
+                }
+                break;
+            case 1: // Send register address
+                if (dtwi->write(&reg, 1) == 1) {
+                    state = 2;
+                }
+                break;
+            case 2: // Send register address
+                if (dtwi->write(&val, 1) == 1) {
+                    state = 3;
+                }
+                break;
+            case 3: // Stop Master
+                if (dtwi->stopMaster()) {
+                    return;
+                }
+                break;
+        }
+    }
+
+}
+
+void TPA2016::setGain(int8_t v) {
+    if (v > 30) {
+        v = 30;
+    }
+    setAmp(5, v);
+}
+
+void TPA2016::enableAmplifier() {
+    pinMode(enable, OUTPUT);
+    digitalWrite(enable, HIGH);
+}
+
+void TPA2016::disableAmplifier() {
+    pinMode(enable, OUTPUT);
+    digitalWrite(enable, LOW);
+}
---
+++ TPA2016.h
@@ -0,0 +1,24 @@
+#ifndef _TPA2016_H
+#define _TPA2016_H
+
+#include <Arduino.h>
+#include <DTWI.h>
+
+class TPA2016 {
+	private:
+        DTWI *dtwi;
+        uint8_t enable;
+
+        void initAmp();
+        void setAmp(uint8_t reg, uint8_t val);
+
+	public:
+		TPA2016(DTWI *d, uint8_t e) : dtwi(d), enable(e) {}
+		TPA2016(DTWI &d, uint8_t e) : dtwi(&d), enable(e) {}
+		void begin();
+        void setGain(int8_t vol);
+        void enableAmplifier();
+        void disableAmplifier();
+};
+
+#endif
